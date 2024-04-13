9cc286d3106cab2e16c8d71eedc74b1d9201f2c0
Sagar_Mallick <sagarmallick711@gmail.com>
2024-04-13 22:08:15 +0530

    (commit-7):add not fond error page in global app folder and custom not found error  conditionally import not found function in reviewId folder page and create not-fond page to render

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFoundReview = () => {
+  return (
+    <div>review not found </div>
+  )
+}
+
+export default NotFoundReview
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import { notFound } from 'next/navigation'
 const Review = ({params}:{
     params:
     {reviewId:string,productId:string}}) => {
+        if(parseInt(params.reviewId)>1000){
+            notFound()
+        }
   return (
     <div>Review {params.reviewId} of product {params.productId}</div>
   )
