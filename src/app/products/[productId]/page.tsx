305f600060ebca80814f45a0b1d1e76a72fa9092
Sagar_Mallick <sagarmallick711@gmail.com>
2024-04-14 16:59:28 +0530

    (commit-12):add routing-metadata in productId page generating dynamic metadata and add object based metadatatitle in root layout where default is default route for all and templete for prefix of meta data applyed on children  and absolute used in about where we want to ignore template metadata object

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import { Metadata } from "next"
 
-const Product = ({params}:{params:{productId:string}}) => {
+type Props={
+  params:{
+    productId:string
+  }
+}
+
+export const generateMetadata= async({params}:Props):Promise<Metadata>=>{
+
+  const title= await new Promise(resolve=>{
+    setTimeout(()=>{
+      resolve(`iphone ${params.productId}`)
+    },1000)
+  })
+
+return{
+  title:`product ${title}`
+}
+}
+
+const Product = ({params}:Props) => {
   return (
     <div>Product list {params.productId}</div>
   )
