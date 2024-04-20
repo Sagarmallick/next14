626127f5c7f8ee76acb54768b2046eaa2ba39dc7
Sagar_Mallick <sagarmallick711@gmail.com>
2024-04-20 13:28:47 +0530

    (commit -17):add template in auth folder at first its an layout page put input field and fill the input field when we navigate to diffrent route its input field is preserve but because layouts not mount and rerender the page when we want to rerender or mout the page we change layout to template it will rerender or remout the page that mean the input field will reset to empty it will not preserve the state

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useState } from "react"
+
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
@@ -9,10 +11,18 @@ const navlinks=[
     {name:'forget password',href:'/password'}
 ]
 
+
+
 const AuthLayout = ({children}:{children:React.ReactNode}) => {
+    const [input, setInput]=useState('')
+
     const pathname=usePathname()
     return (
         <div>
+            <div>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                 value={input} onChange={(e)=>setInput(e.target.value)} />
+            </div>
         {navlinks.map((link)=>{
         const isActive= pathname.startsWith(link.href)
             return(
