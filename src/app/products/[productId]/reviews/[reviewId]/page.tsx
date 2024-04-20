64b252a3032a18ac24b9f82efaca63332a70521e
Sagar_Mallick <sagarmallick711@gmail.com>
2024-04-20 14:08:18 +0530

    (commit-19): add error page in reviewId folder to encounter the error which load the page which is not effect by error the error handle for a specific component which show the error apart from this component page will render perfectlly

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+
 import { notFound } from 'next/navigation'
+
+function getRandomInt(count:number){
+  return Math.floor(Math.random()* count)
+}
+
 const Review = ({params}:{
     params:
-    {reviewId:string,productId:string}}) => {
+    {reviewId:string,
+      productId:string}}) => {
+
+        const random = getRandomInt(2)
+        if(random===1){
+          throw new Error('error loading review')
+        }
+
         if(parseInt(params.reviewId)>1000){
             notFound()
         }
