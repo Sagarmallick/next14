eb0e95d1fabb1074b9acf3cfb1ba55900906abf9
Sagar_Mallick <sagarmallick711@gmail.com>
2024-04-22 23:37:08 +0530

    (commit-27):add Get method route handler in comments folder where we fetch data from data.ts file in comments route handlerin json format

---
+++ src/app/comments/data.ts
@@ -0,0 +1,15 @@
+export const comments=[
+    {
+        id:1,
+        text:"this is first comments"
+    },
+    {
+        id:2,
+        text:"this is second comments"
+    },
+    {
+        id:3,
+        text:"this is third comments"
+    },
+
+]
